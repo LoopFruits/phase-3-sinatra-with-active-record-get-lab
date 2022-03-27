60489a9771fd77b508346f577ed11c8aba976f42
LoopFruits <Kensworldc@gmail.com>
2022-03-27 11:42:14 -0400

    commit tree 127618c729a15c8a8f605a943b0ae19a9083b13d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+#set the content-type header  for all responses
+set :default_content_type, 'application/json'
 
+
+# add routes
+  get  '/bakeries' do 
+    'Hello World'
+    #get all the bakeries from the database 
+    bakeries = Bakery.all 
+    #send them back as a JSON array 
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do 
+    bakeries = Bakery.find(params[:id])
+    bakeries.as_json(only: [:name], include: { baked_goods: { only: [:name, :price ]}})
+  end
 end
